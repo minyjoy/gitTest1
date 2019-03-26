1de847bca8bee6c13443cedf02905e13cf1ff6ba
kim <minyjoy@hotmail.com>
2019-03-26 11:47:38 +0900

    enough

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,7 @@ public class HelloWorld{
 	system.out.print("hello");
 	system.out.print("World");
 	system.out.print("Good Bye!!");
+	system.out.print("whatever I do for you but it is not enough");
+      	
+
 }
